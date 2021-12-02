205c879d328f9b6c21b0e7a8bb4aea82b3bf607c
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-02 09:06:51 +0200

    build widget tree skeleton

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,13 +8,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late String _gestureDetected;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Gestures & Drag and Drop'),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _buildGestureDetector(),
+              const Divider(color: Colors.black, height: 44.0),
+              _buildDraggable(),
+              const Divider(height: 40.0),
+              _buildDragTarget(),
+              const Divider(color: Colors.black),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  void _displayGestureDetected(String gesture) {
+    setState(() => _gestureDetected = gesture);
+  }
+
+  GestureDetector _buildGestureDetector() {
+    return GestureDetector(
+      onTap: () {
+        print('onTap');
+        _displayGestureDetected('onTap');
+      },
+    );
+  }
+
+  Widget _buildDraggable() {
+    return Container();
+  }
+
+  Widget _buildDragTarget() {
+    return Container();
   }
 }
